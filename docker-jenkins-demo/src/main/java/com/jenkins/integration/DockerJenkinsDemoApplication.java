034cc523abacc03d68f965ceed5f9b8222cb8037
Saikumar-sconex <saikumar.medapati@sconex.com>
2025-07-18 16:37:04 +0530

    first push

---
+++ docker-jenkins-demo/src/main/java/com/jenkins/integration/DockerJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ docker-jenkins-demo/src/main/java/com/jenkins/integration/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.jenkins.integration.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public ResponseEntity<String> getHelloMessage() {
+		return ResponseEntity.ok("Hello, Iam GET API");
+	}
+}
